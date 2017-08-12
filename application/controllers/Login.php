5170974af3dae44bb633070bd1f9f5b4859029c3
Michael <tvmichael@meta.ua>
2017-08-12 21:31:11 +0300

    Start - project

---
+++ application/controllers/Login.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+    // Load main page
+    public function index()
+    {
+        print 'Login';
+        //$this->load->view('welcome_message');
+    }
+
+}
---
+++ application/controllers/Teacher.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Teacher extends CI_Controller {
+
+    // Load main page - teacher
+    public function index()
+    {
+        print 'Teacher';
+        //$this->load->view('welcome_message');
+    }
+
+}
