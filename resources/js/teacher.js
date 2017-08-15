7e54985651f80fa59119e7038ebf9f77b0bfee47
Michael <tvmichael@meta.ua>
2017-08-15 23:28:15 +0300

    add resources

---
+++ resources/js/teacher.js
@@ -0,0 +1,20 @@
+;(function () {
+    console.log('TEACHER-JOURNAL');
+    var journal = {action:'', teacher:'', group:'', subject:'', student:'', date:'', mark:''};
+
+    // завантажуємо журнал
+    $('#list-teacher-group a').click(function () {
+        journal.action = 'openJournal';
+        journal.teacher = $(this).attr('data-id-teacher');
+        journal.group = $(this).attr('data-id-group');
+        journal.subject = $(this).attr('data-id-subject');
+        //відправляємо запит на серевер
+        $.get($('#header-journal').attr('data-url'), journal)
+            .done(function (data ) {
+                console.log(data);
+        });
+        console.log(journal);
+    })
+
+
+})();
