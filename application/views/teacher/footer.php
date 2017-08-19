bc9fc7751f8d59cdcaec716cc551c238d8fceb71
Michael <tvmichael@meta.ua>
2017-08-19 23:51:26 +0300

    add css footer

--- application/views/teacher/footer.php
+++ application/views/teacher/footer.php
@@ -1,9 +1,16 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 <!-- FOOTER ------------------------------------------------------------------------- -->
-<footer>
-
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <p class="f-info">TMV.2017</p>
+            </div>
+        </div>
+    </div>
 </footer>
+
 
 <!-- JQUERY -->
 <script src="<?php echo base_url()?>resources/jquery/jquery-3.2.1.min.js" type="text/javascript"></script>
