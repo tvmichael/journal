b3cb08a1dc7f35edf38bc1b6e547482ee92731d0
Michael <tvmichael@meta.ua>
2017-08-25 18:21:41 +0300

    group

---
+++ application/views/admin/group.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');?>
+
+<main class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Список груп</div>
+                <div class="panel-body">
+                    <?php d($groups); ?>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</main>
