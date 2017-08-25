05eeb30ec855e1488144b7b36e89133459e82ae3
Michael TV <imesg.ztdif@gmail.com>
2017-08-25 18:34:16 +0300

    delete base url

--- application/config/config.php
+++ application/config/config.php
@@ -25,9 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 // $config['base_url'] = '';
-//$config['base_url'] = 'http://localhost/journal/';
-$config['base_url'] = 'http://meduch.esy.es/journal/';
-
+$config['base_url'] = 'http://localhost/journal/';
 
 /*
 |--------------------------------------------------------------------------
