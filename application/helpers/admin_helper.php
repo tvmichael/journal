a785750b7b1fdf32ce96db056a75288fb686a4e5
Michael <tvmichael@meta.ua>
2017-09-11 16:19:45 +0300

    helper - admin

--- application/helpers/admin_helper.php
+++ application/helpers/admin_helper.php
@@ -53,27 +53,75 @@ if ( ! function_exists('teacher_working_table'))
 if ( ! function_exists('student_select_table'))
 {
     /**
-     * Student_select_option
+     * Student_select_table
      *
-     * Повертає список студентів відповідної групи, формуючи список "tbody" для table
+     * Повертає список студентів відповідної групи,
+     * формуючи список "tr-td" для tbody таблиці table
      *
      * @mas 	масив записів з БД
-     * @return	повертає tbody
+     * @return	повертає внутрішню чатину tbody
      *
      */
     function student_select_table($mas)
     {
-        // id_student, id_group, id, surname, name, patronymic
-        foreach ($mas as $i){
-            echo "<tr value='", $i['id'], "'>";
-            echo '<td>', $i['surname'], '</td>';
-            echo '<td>', $i['name'], '</td>';
-            echo '<td>', $i['patronymic'], '</td>';
-            echo '<td>';
-            echo "<select>x</select>";
+        if ( count($mas) == 0 ) return;
+        // масив має бути відсортований по ID студента
+        // id_student, surname, name, patronymic, id_group, course, groupe, subgroup
+
+        // додаємо групу до списку, для відповіного студента
+        function l_group($mas, $str){
+            if ( strpos($mas['subgroup'], '1/2') ){$ns='1/2';}
+            elseif ( strpos($mas['subgroup'], '1/3') ){$ns='1/3';}
+            elseif ( strpos($mas['subgroup'], '1/4') ){$ns='1/4';}
+            else {$ns='&nbsp;1&nbsp;';}
+            return $str." <span class='label label-default'>$ns</span>";
+        }
+
+        // додаємо новий рядок таблиці
+        function l_tr($mas, $str, $n){
+            echo "<tr>";
+            echo '<td>', $n, '</td>';
+            echo '<td>', $mas['surname'], '</td>';
+            echo '<td>', $mas['name'], '</td>';
+            echo '<td>', $mas['patronymic'], '</td>';
+            echo '<td>', $mas['course'],' ', $mas['groupe'],' ', $str, '</td>';
+            echo "<td class='text-center'>", "<input type='checkbox' value='", $mas['id_student'], "'>", '</td>';
+            echo "<td class='text-center'>";
+                echo "<a href='", base_url('admin/edit_student'),'?editStudentId=', $mas['id_student'], "'>";
+                echo "<button class='btn btn-default btn-xs' type='button'>";
+                echo "<span class='glyphicon glyphicon-edit' aria-hidden='true'></span>";
+                echo "</button></a>";
             echo '</td>';
             echo "</tr>";
+        };
+        $n = 1;
+        $list_group = '';
+        $id = $mas[0]['id_student'];
+        for($i = 0; $i < count($mas); $i++){
+
+            if ( $i != (count($mas)-1) ) {
+                if ($id != $mas[$i + 1]['id_student']) {
+                    // якщо новий ІД то - новий рядок
+                    $list_group = l_group($mas[$i], $list_group);
+                    l_tr($mas[$i], $list_group, $n);
+
+                    $id = $mas[$i + 1]['id_student'];
+                    $list_group = '';
+                    $n++;
+                } else {
+                    // додаємо групу до списку
+                    $list_group = l_group($mas[$i], $list_group);
+                }
+            }
+            else {
+                // якщо останній запис
+                $list_group = l_group($mas[$i], $list_group);
+                l_tr($mas[$i], $list_group, $n);
+            }
         }
     }
 }
+
+
+
 
