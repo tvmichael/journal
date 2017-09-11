803890bcd54efb566d14498d5028a94bda5c56d0
Michael <tvmichael@meta.ua>
2017-09-11 16:22:00 +0300

    admin - insert_new_students()

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -7,7 +7,7 @@ class Admin_model extends CI_Model
     /** для роботи з викладачами --------------------------------------------------------------- */
     // список усіх викладачів
     public function load_list_teacher(){
-        $q = "SELECT users.id, users.name, users.patronymic, users.surname AS surname, ( SELECT COUNT(list_group_teachers.id_teacher) FROM list_group_teachers WHERE list_group_teachers.id_teacher = users.id ) AS count FROM users WHERE users.role = 'Teacher' GROUP BY users.surname ORDER BY users.surname;";
+        $q = "SELECT *, ( SELECT COUNT(list_group_teachers.id_teacher) FROM list_group_teachers WHERE list_group_teachers.id_teacher = users.id ) AS count FROM users WHERE users.role = 'Teacher' ORDER BY users.surname;";
         $query = $this->db->query($q);
         /*
         $this->db->order_by('surname', 'ASC');
@@ -75,17 +75,48 @@ class Admin_model extends CI_Model
     }
 
     // список студентів коннкретної групи
-    public function readGroupIdStudent(){
-        $data['id_group'] = intval($this->input->get('groupId'));
-        $this->db->select('*');
-        $this->db->from('list_group_students');
-        $this->db->where($data);
-        $this->db->join('students', 'students.id = list_group_students.id_student');
+    public function readStudentAndGroup($id_group){
+        /*
+        SELECT *
+        FROM students
+        INNER JOIN list_group_students
+        ON students.id=list_group_students.id_student
+        INNER JOIN groups
+        ON groups.id = list_group_students.id_group
+        ORDER BY students.id
+        */
+        $this->db->select('id_student, surname, name, patronymic, id_group, course, groupe, subgroup');
+        $this->db->from('students');
+        $this->db->join('list_group_students', 'students.id = list_group_students.id_student');
+        $this->db->join('groups', 'groups.id = list_group_students.id_group');
+        if ($id_group != 0)
+            $this->db->where('groups.id', $id_group);
+        $this->db->order_by('students.id', 'ASC');
         $query = $this->db->get();
 
         return  $query->result_array();
     }
 
+    // додаємо нових студентів до бази
+    public function insert_new_students($data){
+        foreach ($data as $value) {
+            // заносибо студента в БД
+            $sql = ['name'=>$value['name'], 'surname'=>$value['surname'], 'patronymic'=>$value['patronymic']];
+            $this->db->insert('students', $sql);
+
+            // ID номер студента
+            $id = $this->db->insert_id();
+            // заносимо групи до яких належить студентн
+            foreach ($value['group'] as $g){
+                $sql = ['id_student' =>$id, 'id_group'=>$g];
+                $this->db->insert('list_group_students', $sql);
+            }
+        }
+        // тимчасово, просто виводимо масив (треба зробити більш якісну перевірку)
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>";
+    }
 
 
     /** дял роботи з групами ------------------------------------------------------------------- */
@@ -94,6 +125,11 @@ class Admin_model extends CI_Model
         $query = $this->db->get('groups');
         return  $query->result_array();
     }
+
+
+
+
+
 
 
 
@@ -129,7 +165,7 @@ class Admin_model extends CI_Model
         }
     }
     // додаємо нових студентів до бази з ексель файла
-    public function insert_new_students($data){
+    public function insert_new_students_0($data){
         foreach ($data as $value) {
             $sql = ['name'=>$value['name'], 'surname'=>$value['surname'], 'patronymic'=>$value['patronymic']];
             $this->db->insert('students', $sql);
@@ -160,6 +196,24 @@ class Admin_model extends CI_Model
             d($value);
             //$this->db->insert('subjects', $value);
         }
+    }
+
+    //
+    public function table_student_dependence(){
+        $query = $this->db->get('students');
+        $student = $query->result_array();
+
+        $data = [];
+        foreach ($student as $s){
+            //$data[$s['id']] =
+
+            $this->db->select('list_group_students');
+            $this->db->join('groups', 'groups.id = list_group_students.id_group');
+            $this->db->where('list_group_students.id_student', $s['id']);
+            $query = $this->db->get();
+            $list_g = $query->result_array();
+        }
+
     }
 
 
--- application/models/Teacher_model.php
+++ application/models/Teacher_model.php
@@ -23,7 +23,7 @@ class Teacher_model extends CI_Model
         $data['id_teacher'] = intval($this->input->get('teacher'));
         $data['id_group'] = intval($this->input->get('group'));
         $data['id_subject'] = intval($this->input->get('subject'));
-        // перевііряємо чи отримані значення є правильними
+        // перевіряємо чи отримані значення є правильними (чи даний вчитель читає в даних групах і має відповідні предмети)
         if ($data['id_teacher'] != $_SESSION['id'] OR
             !in_array($data['id_group'], $_SESSION['list_group']) OR
             !in_array($data['id_subject'], $_SESSION['list_subject']))
@@ -42,7 +42,7 @@ class Teacher_model extends CI_Model
             $this->db->join('students', 'list_group_students.id_student = students.id');
             $this->db->order_by('surname');
             $query = $this->db->get();
-            $data['students'] = $query->result_array();
+            $data['students'] = $query->result_array(); //список студентів
 
             // отримати журнал викладача, вибираємо при умові = викладач-група-предмет, знахдимо усі дати і сортуємо по даті
             $array = array('id_teacher =' => $data['id_teacher'],
@@ -51,14 +51,21 @@ class Teacher_model extends CI_Model
             $this->db->order_by('date');
             $this->db->select('*');
             $query = $this->db->get_where('journals', $array);
-            $data['journal'] = $query->result_array();
+            $data['journal'] = $query->result_array();  // журнал з оцінками
 
-            // назва (курсу групи підгрупи), для відображення заголовку зверху над таблицею
+            // назва (курсу групи підгрупи), для відображення в заголовку зверху над таблицею
             $this->db->select('course, groupe, subgroup');
             $this->db->from('groups');
             $query = $this->db->where('id', $data['id_group']);
             $query = $this->db->get();
-            $data['groupe'] = $query->row_array();
+            $data['groupe'] = $query->row_array(); // повертає один запис
+
+            // назва предмету для відображення в заголовку зверху над таблицею
+            $this->db->select('shortname, fullname');
+            $this->db->from('subjects');
+            $query = $this->db->where('id', $data['id_subject']);
+            $query = $this->db->get();
+            $data['subject'] = $query->row_array(); // повертає один запис
 
             //список типів занять
             $this->db->select('*');
@@ -103,7 +110,7 @@ class Teacher_model extends CI_Model
         return $query->num_rows();
     }
 
-    // дожаємо оцінку для конкретного студента
+    // додаємо оцінку для конкретного студента
     public function add_new_mark(){
         $data = [];
         $data['id_teacher'] = intval($this->input->get('teacher'));
@@ -188,5 +195,39 @@ class Teacher_model extends CI_Model
         return $s;
     }
 
+    //зберігаємо повідомлення від викладача
+    public function save_teacher_message(){
+        $mas = [];
+        $mas['id_user'] = $_SESSION['id'];
+        $mas['date'] = $this->input->get('date');
+        $mas['message'] = $this->input->get('message');
+        $mas['direction'] = 1; // 1 - від користувача
+        $this->db->insert('message', $mas);
+        $r = $this->db->affected_rows();
+        return $r;
+    }
+
+    // отримати усі повідомлення викладача
+    public function get_all_teacher_message(){
+        $this->db->select('*');
+        $this->db->where('id_user', $_SESSION['id']);
+        $this->db->order_by('date', 'DESC');
+        $query = $this->db->get('message');
+        return $query->result_array();
+    }
+
+
+    // .......
+    public function write_readr_display_type($display_type, $rw){
+        if($rw == 'write'){
+            //
+        }
+        if($rw == 'read'){
+            $this->db->select('display_type');
+            $this->db->where('id_user', $_SESSION['id']);
+            $query = $this->db->get('settings');
+            return $query->row_array();
+        }
+    }
 
 } // end Teacher_model
