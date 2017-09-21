3efeab8c05c3a7de5c310609f76cad9fb596c4eb
Michael <tvmicahel@meta.ua>
2017-09-21 22:57:36 +0300

    main

---
+++ application/views/inspector/main.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<!-- MAIN --------------------------------------------------------------------------- -->
+<main id="inspector-main"
+      data-url="<?php echo base_url('inspector/')?>">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+
+                <h1>dsadsdasdsa</h1>
+
+            </div>
+            <script type="text/javascript">
+                <?php // echo 'var listGT = '.json_encode($list_gt, JSON_UNESCAPED_UNICODE).';'; ?>
+            </script>
+        </div>
+    </div>
+</main>
