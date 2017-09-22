e86ceb8460370e547052b3731602fa87babe60d3
Michael <tvmicahel@meta.ua>
2017-09-22 17:54:25 +0300

    inspector

---
+++ application/controllers/Inspector.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Inspector extends CI_Controller
+{
+    // constructor
+    public function __construct()
+    {
+        parent::__construct();
+        if ( $_SESSION['role'] != 'Inspector' ) redirect(base_url());
+        //модель для роботи з БД
+        $this->load->model('Inspector_model', 'inspector');
+        //
+        $this->load->library('kint/Kint');
+    }
+
+
+    // головна сторніка
+    public function index() {
+        $header = ['navbar_brand'=>'Статистика'];
+        $main = $this->inspector->base_statistics();
+        $footer = ['js_file'=>'inspector-main.js'];
+
+        $this->load->view('inspector/header', $header);
+        $this->load->view('inspector/main', $main);
+        $this->load->view('inspector/footer', $footer);
+    }
+
+}
