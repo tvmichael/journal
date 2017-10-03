779d8bdf0c7f405b0641348ecb9cab128c8f7eb6
Michael <tvmichael@meta.ua>
2017-10-03 08:55:14 +0300

    res.

--- resources/js/inspector-main.js
+++ resources/js/inspector-main.js
@@ -1,29 +1,81 @@
 function l(s) { console.log(s)} l('Inspector start!');
 
-// Пошук викладача за прізвищем
-$('#input-search-teacher').keyup(function () {
-    var filter, table, tbody, tr, td, i;
-    filter = $(this).val();
-    table = document.getElementsByTagName('table')[0];
-    tbody = table.getElementsByTagName('tbody')[0];
-    tr = tbody.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
-        if (td) {
-            if (td.innerHTML.toLowerCase().indexOf(filter.toLowerCase()) > -1) {
+// сторніка
+var namePage = document.getElementsByTagName('main')[0].getAttribute("data-name");
+
+
+// якщо головна сторніка - список викладачів
+if(namePage == 'inspector-main') {
+    // Пошук викладача за прізвищем
+    $('#input-search-teacher').keyup(function () {
+        var filter, table, tbody, tr, td, i;
+        filter = $(this).val();
+        table = document.getElementsByTagName('table')[0];
+        tbody = table.getElementsByTagName('tbody')[0];
+        tr = tbody.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[1];
+            if (td) {
+                if (td.innerHTML.toLowerCase().indexOf(filter.toLowerCase()) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    });
+}
+
+
+// якщо сторінка викладача - з переліком предметів і груп
+if(namePage == 'inspector-teacher') {
+    // Пошук викладача за прізвищем
+    $('#input-search').keyup(function () {
+        var filter, table, tbody, tr, td, i;
+        filter = $(this).val();
+        table = document.getElementsByTagName('table')[0];
+        tbody = table.getElementsByTagName('tbody')[0];
+        tr = tbody.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getAttribute('data-search');
+            if (td.toLowerCase().indexOf(filter.toLowerCase()) > -1) {
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
             }
         }
-    }
-});
+    });
+}
 
-/*
-// Відкрити викладача
-$('table tbody tr').click(function () {
-    //var main = document.getElementById('inspector-main');
-    //document.location.assign(main.getAttribute('data-url')+'teacher?action=openTeacher&id='+$(this).attr('data-id'));
-    //document.location.href = main.getAttribute('data-url')+'teacher?action=openTeacher&id='+$(this).attr('data-id');
-})
-*/
+
+// якщо сторінка журналу для конкретного викладача
+if(namePage == 'inspector-teacher-journal') {
+
+    $('table tbody tr').each(function () {
+
+    });
+
+    l('inspector-teacher-journal');
+}
+
+
+
+// якщо сторінка студентів
+if(namePage == 'inspector-main-students') {
+    // Пошук студента за прізвищем
+    $('#input-search-student').keyup(function () {
+        var filter, table, tbody, tr, td, i;
+        filter = $(this).val();
+        table = document.getElementsByTagName('table')[0];
+        tbody = table.getElementsByTagName('tbody')[0];
+        tr = tbody.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getAttribute('data-search');
+            if (td.toLowerCase().indexOf(filter.toLowerCase()) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    });
+}
