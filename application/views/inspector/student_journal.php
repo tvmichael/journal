f81b912d88e7eda17551621aca5f580771e18a1f
Michael <tvmichael@meta.ua>
2017-10-06 00:18:44 +0300

    sj

---
+++ application/views/inspector/student_journal.php
@@ -0,0 +1,72 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<!-- MAIN --------------------------------------------------------------------------- -->
+<main data-name="inspector-main-student-journals">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+
+
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <?php echo $student_name; ?>
+                    </div>
+                    <div class="panel-body">
+                        <?php foreach ($journal as $j){
+                            echo "<h4><span class='label label-primary'>".
+                                $j[0]['fullname'].
+                                "</span> <span class='label label-default'>".
+                                $j[0]['surname'].' '.$j[0]['name'].
+                                '</span></h4>';
+                            $th = '';
+                            $td = '';
+                            $sb = 0;
+                            $k = 0;
+                            foreach ($j as $val){
+                                $d = explode('-', $val['date']);
+                                $th = $th.'<th>'.$d[2].'<br><span style="color: gray; border-top: 1px solid gray;">'.$d[1].'</span></th>';
+                                $td = $td.'<td>'.$val['mark'].'</td>';
+                                if (intval($val['mark']) > 0 ){
+                                    $sb = $sb +  intval($val['mark']);
+                                    $k++;
+                                }
+                            };
+                            if ($k != 0) { $sb = round($sb / $k, 1);}
+                                else $sb = '';
+
+                            $th = '<th style="width: 5%;">Дата</th><th style="width: 5%;">с.б.</th>'.$th;
+                            $td = '<td>Оцінка</td><td>'.$sb.'</td>'.$td;
+                        ?>
+                        <div style='overflow-x:auto'>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <?php echo $th; ?>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <?php echo $td; ?>
+                                </tr>
+                            </tbody>
+                        </table>
+                        </div>
+                        <br>
+                        <?php }; ?>
+
+                        <?php
+                        //d($subjects);
+                        //d($journal);
+                        $time_end = microtime(true) - $time;
+                        //echo '<br>Time: '.round($time_end,6).'c.';
+                        ?>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    </div>
+</main>
