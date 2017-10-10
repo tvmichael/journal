25f9edc0091bbce4eef984ea75e7c6b17af297c9
Michael <tvmichael@meta.ua>
2017-10-10 23:42:46 +0300

    student

--- application/views/student/main.php
+++ application/views/student/main.php
@@ -6,14 +6,20 @@ defined('BASEPATH') OR exit('No direct script access allowed'); ?>
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-12">
-
-                <h1>STUDENT</h1>
-                <?php
-                d($_SESSION);
-                ?>
-
-
+                <div id="student-data"></div>
             </div>
         </div>
     </div>
 </main>
+<!-- STUDENT OBJECT ----------------------------------------------------------------- -->
+<script>
+    <?php
+    $student = array();
+    $student['id'] = $_SESSION['id'];
+    $student['name'] = $_SESSION['name'];
+    $student['surname'] = $_SESSION['surname'];
+    $student['patronymic'] = $_SESSION['patronymic'];
+    echo 'var student = '.json_encode($student, JSON_UNESCAPED_UNICODE).';';
+    echo 'var journal = '.json_encode($journal, JSON_UNESCAPED_UNICODE).';';
+    ?>
+</script>
