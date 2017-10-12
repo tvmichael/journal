bb82d1ddb78dee575e62f66656f3c441377f1727
Michael <tvmichael@meta.ua>
2017-10-12 23:03:46 +0300

    admin

--- application/models/Student_model.php
+++ application/models/Student_model.php
@@ -6,12 +6,17 @@ class Student_model  extends CI_Model
 
     // журнал студента
     public function load_student_journal(){
+        $id = $_SESSION['id'];
+        //
+        $id = 141;
+
         $q = "
-        SELECT id_teacher, id_subject, id_group, id_lesson_type, lesson_number, mark, remark, date, fullname, name,surname, lesson_type FROM journals
+        SELECT id_teacher, id_subject, id_group, id_lesson_type, lesson_number, mark, remark, date, fullname, name,surname, lesson_type 
+        FROM journals
           JOIN subjects ON subjects.id = id_subject
           JOIN lesson_types ON lesson_types.id = id_lesson_type
           JOIN users ON users.id = id_teacher
-        WHERE id_student=141;
+        WHERE id_student=$id;
         ";
         $query = $this->db->query($q);
 
