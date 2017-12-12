ef20bd411889648f7c6e070827f2353f413da79a
Michael <tvmichael@meta.ua>
2017-12-13 00:05:22 +0200

    php

--- application/controllers/Inspector.php
+++ application/controllers/Inspector.php
@@ -57,7 +57,9 @@ class Inspector extends CI_Controller
     public function student(){
         $header = ['navbar_brand'=>'Статистика'];
         $footer = ['js_file'=>'inspector-main.js'];
+
         if ($this->input->get('action') == 'openStudentJournal') {
+            // сторінка з журналом окремого студента
             $main = $this->inspector->student_statistics_journal();
             $main['student_name'] = $this->input->get('studentName');
 
@@ -65,7 +67,28 @@ class Inspector extends CI_Controller
             $this->load->view('inspector/student_journal', $main);
             $this->load->view('inspector/footer', $footer);
         }
+        elseif ($this->input->get('action') == 'openStudentGroup') {
+            // сторінка зі списком усіх груп
+            $main['group_name'] = 'Список груп';
+            $main['group_table'] = $this->inspector->get_list_all_group();
+
+            $this->load->view('inspector/header', $header);
+            $this->load->view('inspector/student_group', $main);
+            $this->load->view('inspector/footer', $footer);
+        }
+        elseif ($this->input->get('action') == 'openGroup'){
+            // відкрити окрему групу
+            $id = intval($this->input->get('id'));
+            $main = $this->inspector->get_group_statistic($id);
+            $main['group_name'] = $this->input->get('groupName');
+            $main['id_group'] = $id;
+
+            $this->load->view('inspector/header', $header);
+            $this->load->view('inspector/student_current_group', $main);
+            $this->load->view('inspector/footer', $footer);
+        }
         else{
+            // список ісіх студентів
             $main = $this->inspector->student_statistics();
 
             $this->load->view('inspector/header', $header);
