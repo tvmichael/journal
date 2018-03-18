3cae9158444c55f54c735e9db436412b8a3acfa8
tvmichael <tvmichael@meta.ua>
2018-03-18 10:58:49 +0200

    get_group_statistic

--- application/models/Student_model.php
+++ application/models/Student_model.php
@@ -23,7 +23,50 @@ class Student_model  extends CI_Model
         return  $query->result_array();
     }
 
+    public function get_group_statistic($id_student){
+        $data = array();
 
+        $this->db->select('*');
+        $this->db->from('list_group_students');
+        $this->db->where('id_student', $id_student);
+        $query = $this->db->get();
+        //$data['students'] = $query->result_array();
+
+        foreach ($query->result_array() as $value){
+            $id = $value['id_group'];
+
+            $this->db->select('id, name, surname');
+            $this->db->from('students');
+            $this->db->join('list_group_students', 'list_group_students.id_student = students.id');
+            $this->db->where('list_group_students.id_group', $id);
+            $query = $this->db->get();
+            $students = $query->result_array();
+
+            $this->db->select('*');
+            $this->db->from('journals');
+            $this->db->where('id_group', $id);
+            $query = $this->db->get();
+            $journals = $query->result_array();
+
+            $data['data_journal'][] = array($id, $students, $journals);
+
+        }
+        /*
+        $this->db->select('id, name, surname');
+        $this->db->from('students');
+        $this->db->join('list_group_students', 'list_group_students.id_student = students.id');
+        $this->db->where('list_group_students.id_group', $id);
+        $query = $this->db->get();
+        $data['students'] = $query->result_array();
+
+        $this->db->select('*');
+        $this->db->from('journals');
+        $this->db->where('id_group', $id);
+        $query = $this->db->get();
+        $data['journals'] = $query->result_array();
+        /**/
+        return $data;
+    }
 
 
 
